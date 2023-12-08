585e397409fccfc372d181d3e52862dff2908822
Louisfi6 <116919020+Louisfi6@users.noreply.github.com>
2023-12-08 20:00:09 +0100

    Update LightGBM_regressor.py

--- Tree-based Models/LightGBM_regressor.py
+++ Tree-based Models/LightGBM_regressor.py
@@ -5,35 +5,35 @@ from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import LabelEncoder
 from joblib import dump
 
-# Charger le dataset à partir d'un fichier CSV
+#on charge le dataset à partir d'un fichier CSV
 df = pd.read_csv('clean.csv')
 
-# Convertir les caractéristiques catégorielles en valeurs numériques
+#on converti les caractéristiques catégorielles en valeurs numériques
 label_encoder = LabelEncoder()
 df['type'] = label_encoder.fit_transform(df['type'])
 
-# Séparation des caractéristiques et de la cible
+#séparation des caractéristiques et de la cible
 X = df.drop('isFraud', axis=1)
 y = df['isFraud']
 
-# Division en ensembles d'entraînement et de test
+#division en ensembles d'entraînement et de test
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42)
 
-# Création du modèle LightGBM Regressor
+#création du modèle LightGBM Regressor
 model = LGBMRegressor(n_estimators=100, random_state=42)
 
-# Entraînement du modèle
+#entraînement du modèle
 model.fit(X_train, y_train)
 
-# Prédictions sur l'ensemble de test
+#prédictions sur l'ensemble de test
 y_pred = model.predict(X_test)
 
-# Évaluation du modèle
+#évaluation du modèle
 mse = mean_squared_error(y_test, y_pred)
 
-# Imprimer l'erreur quadratique moyenne
+#imprimer l'erreur quadratique moyenne (pour la pécision)
 print("Erreur quadratique moyenne :", mse)
 
-# Enregistrer le modèle
+#enregistrer le modèle
 dump(model, 'Modele_LightGBMRegressor.joblib')
