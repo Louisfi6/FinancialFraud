29a3d16c2bc924b57dee0340a8559d8eff236dad
Louisfi6 <116919020+Louisfi6@users.noreply.github.com>
2023-12-08 20:02:36 +0100

    Update decision_tree.py

--- Tree-based Models/decision_tree.py
+++ Tree-based Models/decision_tree.py
@@ -5,36 +5,36 @@ from sklearn.metrics import accuracy_score, classification_report
 from sklearn.preprocessing import LabelEncoder
 from joblib import dump
 
-# Charger le dataset à partir d'un fichier CSV
+#on charge le dataset à partir d'un fichier CSV
 df = pd.read_csv('clean.csv')
 
-# Convertir les caractéristiques catégorielles en valeurs numériques
+#on converti les caractéristiques catégorielles en valeurs numériques
 label_encoder = LabelEncoder()
 df['type'] = label_encoder.fit_transform(df['type'])
 
-# Séparation des caractéristiques et de la cible
+#séparation des caractéristiques et de la cible
 X = df.drop('isFraud', axis=1)
 y = df['isFraud']
 
-# Division en ensembles d'entraînement et de test
+#division en ensembles d'entraînement et de test
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42)
 
-# Création du modèle Decision Tree Classifier
+#création du modèle Decision Tree Classifier
 model = DecisionTreeClassifier(random_state=42)
 
-# Entraînement du modèle
+#entraînement du modèle
 model.fit(X_train, y_train)
 
-# Prédictions sur l'ensemble de test
+#prédictions sur l'ensemble de test
 y_pred = model.predict(X_test)
 
-# Évaluation du modèle
+#évaluation du modèle
 precision = accuracy_score(y_test, y_pred)
 
-# Imprimer le rapport de classification et la précision
+#imprimer le rapport de classification et la précision
 print("Rapport de classification :\n", classification_report(y_test, y_pred))
 print("Précision :", precision)
 
-# Enregistrer le modèle
+#enregistrer le modèle
 dump(model, 'Modele_DecisionTree.joblib')
